266e76e875ba3e404f14b83786a65bff5b811dfd
arnab-ray <arnab.ray@udaan.com>
2023-01-08 16:19:23 +0530

    added funstacker

--- stacker.rkt
+++ stacker.rkt
@@ -3,12 +3,33 @@
 (define (read-syntax path port)
   (define src-lines (port->lines port))
   (define src-datums (format-datums '(handle ~a) src-lines))
-  (define module-datum `(module stacker-mod br
+  (define module-datum `(module stacker-mod "stacker.rkt"
                           ,@src-datums))
   (datum->syntax #f module-datum))
 (provide read-syntax)
 
 (define-macro (stacker-module-begin HANDLE-EXPR ...)
   #'(#%module-begin
-     'HANDLE-EXPR ...))
+     HANDLE-EXPR ...
+     (display (first stack))))
 (provide (rename-out [stacker-module-begin #%module-begin]))
+
+(define stack empty)
+
+(define (pop-stack!)
+  (define arg (first stack))
+  (set! stack (rest stack))
+  arg)
+
+(define (push-stack! arg)
+  (set! stack (cons arg stack)))
+
+(define (handle [arg #f])
+  (cond
+    [(number? arg) (push-stack! arg)]
+    [(or (equal? + arg) (equal? * arg))
+     (define result (arg (pop-stack!) (pop-stack!)))
+     (push-stack! result)]))
+(provide handle)
+
+(provide + *)
